d51b3a2a939fcaaa8cbf8623cb23eab403c09aa4
paulo-rfs <paulo.furlan.silva@escola.pr.gov.br>
2026-08-04 23:03:52 -0300

    .

---
+++ script.js
@@ -0,0 +1,25 @@
+const botao = document.querySelector("button");
+botao.addEventListener("click", botaoClicado);
+
+function botaoClicado() {
+let texto = botao.querySelector("span");
+texto.textContent++;
+}
+
+botoes.forEach(function (botao) {
+let curtiu = false;
+
+botao.addEventListener("click", botaoClicado);
+
+function botaoClicado() {
+console.log("fui clicado");
+let texto = botao.querySelector("span");
+
+if (curtiu === false) {
+texto.textContent++;
+curtiu = true;
+} else {
+texto.textContent--;
+curtiu = false;
+}
+});
